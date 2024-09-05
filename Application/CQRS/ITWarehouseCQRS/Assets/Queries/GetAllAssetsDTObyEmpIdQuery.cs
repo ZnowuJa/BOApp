8dd79332d6527fff2cf5decbead21632e87fa8a0
Marcin <marcin.jarco@porscheinterauto.pl>
2024-09-05 15:59:52 +0200

    New CQRS getter for AssetDTO, changes for DeferralPaymentEdit, ITWareHouse for Users.

---
+++ Application/CQRS/ITWarehouseCQRS/Assets/Queries/GetAllAssetsDTObyEmpIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+
+using MediatR;
+
+namespace Application.ITWarehouseCQRS.Assets.Queries;
+public class GetAllAssetsDTObyEmpIdQuery : IRequest<IQueryable<AssetDTO>>
+{
+    public int uId {  get; set; } 
+
+    public GetAllAssetsDTObyEmpIdQuery(int _uId)
+    {
+        uId = _uId;
+
+    }
+}
