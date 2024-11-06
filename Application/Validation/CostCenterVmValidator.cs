5e18e2429e895658a5556d0955e4a879f2d66597
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2024-11-06 10:05:55 +0100

    Add Edit_Component and List_Page

---
+++ Application/Validation/CostCenterVmValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.ViewModels;
+using Application.ViewModels.Accounting;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validation
+{
+    public class CostCenterVmValidator : AbstractValidator<CostCenterVm>
+    {
+        public CostCenterVmValidator()
+        {
+            RuleFor(x => x.MPK).NotEmpty();
+            RuleFor(x => x.Description).MinimumLength(3).WithMessage("Description needs to be at least 3 chars!");
+            RuleFor(x => x.Text).NotEmpty();
+        }
+    }
+}
