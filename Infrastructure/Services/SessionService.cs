60cd33c655ca6cf65dc4be549ee5ee0a0658fbbd
Marcin <marcin.jarco@porscheinterauto.pl>
2024-11-28 23:04:04 +0100

    Additional logger in SessionService.cs

--- Infrastructure/Services/SessionService.cs
+++ Infrastructure/Services/SessionService.cs
@@ -1,14 +1,16 @@
 ﻿using Application.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Services;
 public class SessionService : ISessionService
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public SessionService(IHttpContextAccessor httpContextAccessor)
+    private readonly ILogger<SessionService> _logger;
+    public SessionService(IHttpContextAccessor httpContextAccessor, ILogger<SessionService> logger)
     {
         _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
     }
 
     public string GetSessionId()
@@ -16,11 +18,13 @@ public class SessionService : ISessionService
         var context = _httpContextAccessor.HttpContext;
         if (context == null)
         {
+            _logger.LogWarning("SESSION_Service :: HttpContext is null.");
             return null; // HttpContext might be null in some scenarios
         }
 
         // Ensure a session is started
-        context.Session.SetString("SessionStarted", "true");
-        return context.Session.Id;
+        var sessionId = context.Session.Id;
+        _logger.LogInformation($"Session ID: {sessionId}");
+        return sessionId;
     }
 }
