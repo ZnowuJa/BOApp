d23c503e3f3680539ec284bf75c6ad11a63f5d7e
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2024-12-16 15:21:40 +0100

    Set DatePicker Validation

--- Application/CQRS/AccountingCQRS/BNP/Queries/GetAllBnp20Query.cs
+++ Application/CQRS/AccountingCQRS/BNP/Queries/GetAllBnp20Query.cs
@@ -4,28 +4,29 @@ using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace Application.CQRS.AccountingCQRS.BNP.Queries;
-
-public class GetAllBnp20Query(DateOnly startDate, DateOnly endDate) : IRequest<IQueryable<Bnp20Vm>>
+namespace Application.CQRS.AccountingCQRS.BNP.Queries
 {
-    //public string Bnp { get; set; }
-    public DateOnly StartDate { get; set; } = startDate;
-    public DateOnly EndDate { get; set; } = endDate;
-}
-
-public class GetAllBnp20QueryHandler(IBNPDbContext bnpDbContext, IMapper mapper)
-    : IRequestHandler<GetAllBnp20Query, IQueryable<Bnp20Vm>>
-{
-    private readonly IBNPDbContext _bnpDbContext = bnpDbContext;
-    private readonly IMapper _mapper = mapper;
-
-    public async Task<IQueryable<Bnp20Vm>> Handle(GetAllBnp20Query request, CancellationToken cancellationToken)
+    public class GetAllBnp20Query(DateOnly startDate, DateOnly endDate) : IRequest<IQueryable<Bnp20Vm>>
     {
-        var bnp20s = await _bnpDbContext.Bnp20s.Where(i => i.Data >= request.StartDate && i.Data <= request.EndDate)
-            .AsNoTracking()
-            .ToListAsync(cancellationToken);
+        //public string Bnp { get; set; }
+        public DateOnly StartDate { get; set; } = startDate;
+        public DateOnly EndDate { get; set; } = endDate;
+    }
 
-        var bnp20Vms = _mapper.Map<List<Bnp20Vm>>(bnp20s);
-        return bnp20Vms.AsQueryable();
+    public class GetAllBnp20QueryHandler(IBNPDbContext bnpDbContext, IMapper mapper)
+        : IRequestHandler<GetAllBnp20Query, IQueryable<Bnp20Vm>>
+    {
+        private readonly IBNPDbContext _bnpDbContext = bnpDbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<IQueryable<Bnp20Vm>> Handle(GetAllBnp20Query request, CancellationToken cancellationToken)
+        {
+            var bnp20s = await _bnpDbContext.Bnp20s.Where(i => i.Data >= request.StartDate && i.Data <= request.EndDate)
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            var bnp20Vms = _mapper.Map<List<Bnp20Vm>>(bnp20s);
+            return bnp20Vms.AsQueryable();
+        }
     }
 }
