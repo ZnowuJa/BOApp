4e4df5a92d2c57fc52bdffdc977cdcb2a9989ee2
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2025-02-03 14:11:47 +0100

    Adding ListPage and CQRS for BackgroundJobs

---
+++ Application/CQRS/General/BackgroundJobs/Queries/GetAllBackgroundJobQuery.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Application.ViewModels.General;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.General.BackgroundJobs.Queries
+{
+    public class GetAllBackgroundJobQuery : IRequest<IQueryable<BackgroundJobVm>>
+    {
+    }
+
+    public class GetAllBackgorundJobQueryHandler(IAppDbContext appDbContext, IMapper mapper) : IRequestHandler<GetAllBackgroundJobQuery, IQueryable<BackgroundJobVm>>
+    {
+        private readonly IAppDbContext _appDbContext = appDbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<IQueryable<BackgroundJobVm>> Handle(GetAllBackgroundJobQuery query, CancellationToken cancellationToken)
+        {
+            var backgroundJobs = await _appDbContext.BackgroundJobs.ToListAsync(cancellationToken);
+            var backgroundJobsVms = _mapper.Map<List<BackgroundJobVm>>(backgroundJobs);
+            return backgroundJobsVms.AsQueryable();
+        }
+    }
+}
---
+++ Application/ViewModels/General/BackgroundJobVm.cs
@@ -0,0 +1,21 @@
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities.Administration;
+
+namespace Application.ViewModels.General
+{
+    public class BackgroundJobVm : IMapFrom<BackgroundJob>
+    {
+        public int Id { get; set; }
+        public string AssemblyName { get; set; }
+        public string JobClass { get; set; }
+        public string JobMethod { get; set; }
+        public string CronExpression { get; set; }
+        public bool Enabled { get; set; }
+        public string IsEnabled => Enabled ? "Tak" : "Nie";
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<BackgroundJob, BackgroundJobVm>().ReverseMap();
+        }
+    }
+}
