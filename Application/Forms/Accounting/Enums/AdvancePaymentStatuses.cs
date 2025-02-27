48013c389b1ab4dd1ef681317369e010e1778245
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2025-02-27 12:08:10 +0100

    Change statuses for AdvancePayment

--- Application/Forms/Accounting/Enums/AdvancePaymentStatuses.cs
+++ Application/Forms/Accounting/Enums/AdvancePaymentStatuses.cs
@@ -10,12 +10,14 @@ namespace Application.Forms.Accounting.Enums
     {
         Rejestracja, //user or just employee
         AprobataL1, // manager of user
-        AprobataL2, // manager of manager of user
-        Ksiegowosc, // Accountants
+        AprobataL2, //dopytać księgowe
+        ZaliczkaKasa,
+        ZaliczkaKsiegowosc, // Accountants
         WyslaneDoRobota, // BOT
-        KsiegowoscTL, // Accountants TLs
-        Kasa, // Cashier
-        Zamkniete, // close
-        Odrzucone, // reject
+        BladRobota, // Księgowa
+        ZaliczkaKsiegowoscTL, // Accountants TLs
+        Odrzucone,
+        Wyplacone,
+        Rozliczone
     }
 }
