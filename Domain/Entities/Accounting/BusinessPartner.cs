51f1da9b652728413996e5df1eca5a5d67c88a33
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2025-03-27 15:52:37 +0100

    Template for BusinessPartner

---
+++ Domain/Entities/Accounting/BusinessPartner.cs
@@ -0,0 +1,22 @@
+﻿using BackOfficeApp_Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Accounting
+{
+    public class BusinessPartner : AuditableEntity
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Branch { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        // Relacja do klasy Country
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
