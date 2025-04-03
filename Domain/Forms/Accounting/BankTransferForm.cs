4f77d497b26412920f0056d0343e0d3ffab76c34
Marcin <marcin.jarco@porscheinterauto.pl>
2025-04-03 09:44:41 +0200

    recover from mess

--- Domain/Forms/Accounting/BankTransferForm.cs
+++ Domain/Forms/Accounting/BankTransferForm.cs
@@ -8,11 +8,53 @@ public class BankTransferForm : FormTemplate
     {
         Statuses = GetDefaultStatuses();
     }
+    
+        public string EnovaEmpId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public string LVL1_EnovaEmpId { get; set; }
+        public string LVL1_EmployeeName { get; set; }
+        public string LVL2_EnovaEmpId { get; set; }
+        public string LVL2_EmployeeName { get; set; }
+        public string LVL3_EnovaEmpId { get; set; }
+        public string LVL3_EmployeeName { get; set; }
+        public string LVL4_EnovaEmpId { get; set; }
+        public string LVL4_EmployeeName { get; set; }
+        public string LVL5_EnovaEmpId { get; set; }
+        public string LVL5_EmployeeName { get; set; }
+
+        public string Level1Approvers { get; set; }
+        public string Level2Approvers { get; set; }
+        public string Level3Approvers { get; set; }
+        public string Level4Approvers { get; set; }
+        public string Level5Approvers { get; set; }
+        public string FormFiles { get; set; }
+        
+        public string OrganisationSapNumber { get; set; } = string.Empty;
+        public string EmployeeSapCostCenterVm { get; set; } = string.Empty;
+        
+        public string FormCostCenters = string.Empty;
+        public string Approvals { get; set; } = string.Empty;
+        public string RejectReasons { get; set; } = string.Empty;
+        public string RejectReason { get; set; } = string.Empty;
+    
+
+    
+        public string Document { get; set; }
+        public string BankTransferTitle { get; set; }
+        public bool SplitPayment { get; set; }
+        
+        public string Notes { get; set; }
+        
+        public string InvoiceMappings { get; set; }
+        public string BankTransferMapping { get; set; }
+    
+ 
+
     public static List<string> GetDefaultStatuses()
     {
         return new List<string>
         {
-            "Rejestracja", "AprobataL1", "AprobataL2", "AprobataL3", "AprobataL4","Ksiegowosc", "KsiegowoscTL",  "Kasa", "WyslaneDoRobota", "BladRobota", "Rozliczone", "Zamkniete", "Odrzucone"
+            "Rejestracja", "AprobataL1", "AprobataL2", "AprobataL3", "Ksiegowosc", "KsiegowoscTL",  "Kasa", "WyslaneDoRobota", "BladRobota", "Rozliczone", "Zamkniete", "Odrzucone"
         };
     }
 }
