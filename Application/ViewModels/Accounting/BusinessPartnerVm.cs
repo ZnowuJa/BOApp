6636e0cc713d5d016f14bd6855e88efa2c61aba2
Marcin <marcin.jarco@porscheinterauto.pl>
2025-05-16 09:17:54 +0200

    Commit changes locally before merge from origin.

--- Application/ViewModels/Accounting/BusinessPartnerVm.cs
+++ Application/ViewModels/Accounting/BusinessPartnerVm.cs
@@ -25,6 +25,7 @@ public class BusinessPartnerVm : IMapFrom<BusinessPartner>
     public string? SAPId { get; set; } = string.Empty;
     public BusinessPartnerType Type { get; set; }
     public string? Location { get; set; } = string.Empty;
+    public bool Selected { get; set; } = false;
 
     public void Mapping(Profile profile)
     {
