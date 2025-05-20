fe3b0c7f1625d204128a1cfc55236831d61cfa21
Urbaniak Dawid (PIAPL - PL Warsaw) <dawid.urbaniak@porscheinterauto.pl>
2025-05-20 13:52:07 +0200

    Changes Validator and EditPage for BusinessPartner

--- Application/Validation/Accounting/BusinessPartnerVmValidator.cs
+++ Application/Validation/Accounting/BusinessPartnerVmValidator.cs
@@ -1,4 +1,5 @@
-﻿using Application.ViewModels.Accounting;
+﻿using Application.AdHocJobs;
+using Application.ViewModels.Accounting;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
@@ -13,31 +14,36 @@ namespace Application.Validation.Accounting
         public BusinessPartnerVmValidator()
         {
             RuleFor(x => x.Name)
+                .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("Pole 'Nazwa' jest wymagane.")
                 .MinimumLength(3).WithMessage("Nazwa musi mieć co najmniej 3 znaki.")
                 .MaximumLength(100).WithMessage("Nazwa może mieć maksymalnie 100 znaków.");
 
             RuleFor(x => x.Street)
+                .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("Ulica jest wymagana.")
                 .MinimumLength(3).WithMessage("Ulica musi mieć co najmniej 3 znaki.")
                 .MaximumLength(100).WithMessage("Ulica może mieć maksymalnie 100 znaków.");
 
             RuleFor(x => x.City)
+                .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("Miasto jest wymagane.")
                 .MinimumLength(3).WithMessage("Miasto musi mieć co najmniej 3 znaki.")
                 .MaximumLength(50).WithMessage("Miasto może mieć maksymalnie 50 znaków.");
 
             RuleFor(x => x.PostalCode)
+                .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("Kod pocztowy jest wymagany.")
                 .Matches(@"^\d{2}-\d{3}$").WithMessage("Kod pocztowy musi być w formacie XX-XXX.");
 
             RuleFor(x => x.Country)
-                .MinimumLength(2).When(x => !string.IsNullOrWhiteSpace(x.Country)).WithMessage("Kraj musi mieć co najmniej 2 znaki.")
-                .MaximumLength(50).WithMessage("Kraj może mieć maksymalnie 50 znaków.");
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Kraj jest wymagany.");
 
             RuleFor(x => x.BankAccountNumber)
-                .MinimumLength(10).When(x => !string.IsNullOrWhiteSpace(x.BankAccountNumber)).WithMessage("Numer konta musi mieć co najmniej 10 znaków.")
-                .MaximumLength(34).WithMessage("Numer konta może mieć maksymalnie 34 znaki.");
+                          .MustAsync(async (iban, cancellation) => await AppUtils.ValidateIbanAsync(iban))
+                          .WithMessage("Nieprawidłowy numer IBAN.")
+                          .When(x => !string.IsNullOrWhiteSpace(x.BankAccountNumber));
 
             RuleFor(x => x.VatId)
                 .MinimumLength(6).When(x => !string.IsNullOrWhiteSpace(x.VatId)).WithMessage("VAT ID musi mieć co najmniej 6 znaków.")
@@ -47,11 +53,17 @@ namespace Application.Validation.Accounting
                 .MinimumLength(4).When(x => !string.IsNullOrWhiteSpace(x.SAPId)).WithMessage("SAP ID musi mieć co najmniej 4 znaki.")
                 .MaximumLength(20).WithMessage("SAP ID może mieć maksymalnie 20 znaków.");
 
-/*            RuleFor(x => x.Type)
-                .IsInEnum().WithMessage("Musisz wybrać poprawny typ partnera.");*/
+            RuleFor(x => x.Type)
+                .NotEmpty().WithMessage("Typ musi zostać wybrany.").NotEqual("Select...").WithMessage("Typ musi zostać wybrany.");
 
             RuleFor(x => x.Location)
-                .MaximumLength(10).WithMessage("Lokalizacja może mieć maksymalnie 10 znaków.");
+                .NotEmpty().WithMessage("Lokalizacja musi zostać wybrana.");
+
+            RuleFor(x => x.SAPFormType)
+                .NotEmpty().WithMessage("SAPFormType musi zostać wybrany.").NotEqual("Select...").WithMessage("SAPFormType musi zostać wybrany.");
+
+            RuleFor(x => x.DefaultCurrency)
+                .NotEmpty().WithMessage("Waluta musi zostać wybrana.").NotEqual("Select...").WithMessage("Waluta musi zostać wybrana.");
         }
     }
 }
