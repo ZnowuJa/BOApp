390757ba9106bf4be888f5d10b6470975c4f9fbe
Marcin <marcin.jarco@porscheinterauto.pl>
2025-05-22 16:00:56 +0200

    fake change

--- Application/ViewModels/Accounting/BusinessPartnerVm.cs
+++ Application/ViewModels/Accounting/BusinessPartnerVm.cs
@@ -26,7 +26,7 @@ public class BusinessPartnerVm : IMapFrom<BusinessPartner>
     public string? Location { get; set; } = string.Empty;
     public string SAPFormType { get; set; } = string.Empty;
     public string DefaultCurrency { get; set; } = string.Empty;
-    public bool Selected { get; set; } = false;
+    public bool Selected { get; set; } = true;
 
     public void Mapping(Profile profile)
     {
